386a1515a1bc4bfbb6a2aad3fe49ee04379c66b9
unknown <jaydeepvishwakarma38@gmail.com>
2023-04-09 01:09:39 +0530

    backend api

--- utils/database.js
+++ utils/database.js
@@ -5,6 +5,6 @@ export const connectDB = () => {
         .connect(process.env.MONGO_URI, {
             dbName: "backendapi",
         })
-        .then(() => console.log("Database Connect...."))
+        .then((c) => console.log(`Database Connect ${c.connection.host}`))
         .catch((e) => console.log(e));
 }
